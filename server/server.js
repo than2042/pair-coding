1f8577b2ddd9235acd61167863d1aa8e4a7ad3da
than2042 <than.than2042@gmail.com>
2024-01-17 11:59:53 +0000

    set up server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+import dotenv from "dotenv";
+
+const app = express();
+const PORT = 8080;
+app.use(express.json());
+app.use(cors());
+
+const dbConnectionString = process.env.DATABASE_URL;
+const db = new pg.Pool({ connectionString: dbConnectionString });
+
+app.listen(PORT, `App is running on ${PORT}`);
