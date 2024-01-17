f30599a9b27d33960615c2b0a144a9d3dc743d25
II-Paulus-II <paulus.coding@gmail.com>
2024-01-17 13:50:04 +0000

    now we have the messages in the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+
+let SERVERNAME = "http://localhost:8080";
 
 function App() {
 
+  //const response = await fetch ( `${SERVERNAME}` );
+  //const jsonResponse = await response.json();
+
+  const [messages, setMessages] = useState([]);
+  //setMessages([...jsonResponse]);
+  //const messages = await response.json();
+
+  useEffect(() => {
+    handleGetMessages();
+    }, []);
+
+  async function handleGetMessages() {
+    // make a fetch request to my api
+    const response = await fetch(`${SERVERNAME}`);
+    const data = await response.json();
+
+    // set todoItems to be the response
+    setMessages(data);
+  }
+
+  console.log(messages);
+
   return (
     <>
+    <div>Hello 
+    </div>
     </>
-  )
+  );
 }
 
 export default App
